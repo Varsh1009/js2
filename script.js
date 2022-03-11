6f7af20ce86d5ea4f3cc4ff386c7ffe7aca50936
Varsh1009 <83211221+Varsh1009@users.noreply.github.com>
2022-03-11 13:04:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let Str="Skill Safari"; // String 
+console.log(Str);
+let len = Str.length;
+console.log(len);
+let result = Str.toLocaleUpperCase();
+console.log(result);
+
+let index = Str.indexOf("a");
+console.log(index);
+console.log("Steve Rogers/Captain America: I'm sorry Tony.... I wouldn't do this if I had any other choice... but he's my friend.\nTony Stark/Iron Man: So was I.\nSteve Rogers/Captain America: If I see a situation pointed south, I can't ignore it. Sometimes I wish I could.\nTony Stark/Iron Man: Sometimes I want to punch you in your perfect teeth.");
+
+
+let height = prompt("Please enter your height");
+let weight = prompt("Please enter your weight");
+let bmi=weight/(height*height);
+console.log(bmi);
